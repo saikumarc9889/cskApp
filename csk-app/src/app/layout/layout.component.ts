b9a960a72004d193565e76453b32ad8bc3c040a0
Sai Kumar <sachebrolu@deloitte.com>
2025-07-20 20:01:19 +0530

    c

---
+++ csk-app/src/app/layout/layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cmp-layout',
+  imports: [],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.scss'
+})
+export class LayoutComponent {
+
+}
